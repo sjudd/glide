90eb503aef70820562e743f5dd18cad7f6492495
Sam Judd <judds@google.com>
2017-10-20 09:37:46 -0700

    Update formatting in unit and samples scripts.

--- scripts/travis_samples.sh
+++ scripts/travis_samples.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-./gradlew :samples:flickr:build :samples:giphy:build :samples:contacturi:build :samples:gallery:build :samples:imgur:build :samples:svg:build --parallel
+./gradlew :samples:flickr:build \
+  :samples:giphy:build \
+  :samples:contacturi:build \
+  :samples:gallery:build \
+  :samples:imgur:build \
+  :samples:svg:build \
+  --parallel
--- scripts/travis_unit.sh
+++ scripts/travis_unit.sh
@@ -2,4 +2,11 @@
 
 set -e
 
-./gradlew clean build -x :samples:flickr:build -x :samples:giphy:build -x :samples:contacturi:build -x :samples:gallery:build -x :samples:imgur:build -x :samples:svg:build -x :library:testReleaseUnitTest --parallel
+./gradlew build \
+  -x :samples:flickr:build \
+  -x :samples:giphy:build \
+  -x :samples:contacturi:build \
+  -x :samples:gallery:build \
+  -x :samples:imgur:build \
+  -x :samples:svg:build \
+  -x testReleaseUnitTest --parallel
