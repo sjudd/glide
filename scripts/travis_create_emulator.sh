00dbafd00d5200d2384ccb9e507af597fecbf661
Sam Judd <judds@google.com>
2017-12-21 23:35:15 -0800

    remove gnu and avoid updating.

--- scripts/travis_create_emulator.sh
+++ scripts/travis_create_emulator.sh
@@ -3,13 +3,13 @@
 set -e
 
 target="system-images;android-${ANDROID_TARGET};default;armeabi-v7a"
-echo y | sdkmanager --update
+#echo y | sdkmanager --update
 echo y | sdkmanager --install $target
 which sdkmanager
 which avdmanager
 sdkmanager --version
 sdkmanager --list | grep emulator
 avdmanager create avd --force -n test -k $target --device "Nexus 5X" -c 2048M
-QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 2048 -gpu swiftshader_indirect &
+QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 2048 &
 
 exit 0
