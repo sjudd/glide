03a03e3c6e0e51681bcd92d5df007123836e34f4
Sam Judd <judds@google.com>
2017-12-21 21:24:24 -0800

    revert engine classic.

--- scripts/travis_create_emulator.sh
+++ scripts/travis_create_emulator.sh
@@ -6,6 +6,6 @@ target="system-images;android-${ANDROID_TARGET};default;armeabi-v7a"
 echo y | sdkmanager --update
 echo y | sdkmanager --install $target
 avdmanager create avd --force -n test -k $target --device "Nexus 5X" -c 1024M
-QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 1024 -engine classic &
+QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 1024 &
 
 exit 0
