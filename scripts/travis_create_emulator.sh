b062f3766426d017aea7b11ecab70aeef4dda5ba
Sam Judd <judds@google.com>
2017-12-21 18:37:31 -0800

    Try engine classic.

--- scripts/travis_create_emulator.sh
+++ scripts/travis_create_emulator.sh
@@ -6,6 +6,6 @@ target="system-images;android-${ANDROID_TARGET};default;armeabi-v7a"
 echo y | sdkmanager --update
 echo y | sdkmanager --install $target
 avdmanager create avd --force -n test -k $target --device "Nexus 5X" -c 2000M
-QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 2048 &
+QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 2048 -engine classic &
 
 exit 0
