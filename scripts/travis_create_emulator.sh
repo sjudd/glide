c8f7e0971289508ff0711accd0d4a653d043597a
Sam Judd <judds@google.com>
2017-12-21 23:41:03 -0800

    Remove —device

--- scripts/travis_create_emulator.sh
+++ scripts/travis_create_emulator.sh
@@ -3,13 +3,13 @@
 set -e
 
 target="system-images;android-${ANDROID_TARGET};default;armeabi-v7a"
-#echo y | sdkmanager --update
+echo y | sdkmanager --update
 echo y | sdkmanager --install $target
 which sdkmanager
 which avdmanager
 sdkmanager --version
 sdkmanager --list | grep emulator
-avdmanager create avd --force -n test -k $target --device "Nexus 5X" -c 2048M
+echo no | avdmanager create avd --force -n test -k $target -c 2048M --abi default/armeabi-v7a
 QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 2048 &
 
 exit 0
