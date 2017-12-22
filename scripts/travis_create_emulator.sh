475f25795d9f7fe5981e6dd481fbbab9526955f5
Sam Judd <judds@google.com>
2017-12-21 22:49:18 -0800

    try force 32 bit, remove flags that don’t seem to matter.

--- scripts/travis_create_emulator.sh
+++ scripts/travis_create_emulator.sh
@@ -5,9 +5,11 @@ set -e
 target="system-images;android-${ANDROID_TARGET};default;armeabi-v7a"
 echo y | sdkmanager --update
 echo y | sdkmanager --install $target
+which sdkmanager
+which avdmanager
 sdkmanager --version
 sdkmanager --list | grep emulator
 avdmanager create avd --force -n test -k $target --device "Nexus 5X" -c 1024M
-QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 1024 -no-snapshot -camera-back none -camera-front none -selinux disabled -nojni &
+QEMU_AUDIO_DRV=none $ANDROID_HOME/emulator/emulator -avd test -no-window -memory 1024 -no-snapshot -force-32bit &
 
 exit 0
